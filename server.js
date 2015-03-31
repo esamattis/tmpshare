2e87240f6eda21588753cca4dbce0e193e858c92
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-03-31 20:13:38 +0300

    add --help

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 "use strict";
 
 var express = require("express");
+var path = require("path");
 var http = require("http");
 var argv = require('minimist')(process.argv.slice(2), {
     alias: {
         p: "port",
+        h: "help",
         b: "base"
     }
 });
 
+var dir = argv._[0];
+
+function help() {
+    console.log("usage: " + path.basename(__filename) + " [--port=3000] [--base=/] <dir>");
+}
+
+if (argv.help) {
+    help();
+    process.exit(0);
+}
+
+if (!dir) {
+    help();
+    process.exit(1);
+}
+
 var app = express();
 
-var tmpshare = require("./index")({
-    dir: argv._[0]
-});
+var tmpshare = require("./index")({dir: dir});
 
 if (argv.base) {
     app.use(argv.base, tmpshare);
