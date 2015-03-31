cc8c37908993148657ffea6460d5da2cb582d4e2
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-03-31 22:44:20 +0300

    Chrome does not like 0.0.0.0 url

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ if (argv.base) {
 http.createServer(app).listen(argv.port || 3000, function(err) {
     if (err) throw err;
     var addr = this.address();
-    console.log("Listening on http://%s:%d", addr.address, addr.port);
+    console.log("Listening on http://localhost:%d", addr.port);
 });
